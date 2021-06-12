78ce805c016f0772ee654268e1428df27d71b93f
olivialiu <olivialiu@soulcalc.com>
2021-06-12 04:50:14 -0400

    Correction

--- src/com/jieqing/TestProgression.java
+++ src/com/jieqing/TestProgression.java
@@ -7,12 +7,12 @@ public class TestProgression
 	public static void main(String[] args)
 	{
 		Progression Prog; 
-		// Test Hw1Progression with default constructor
+		// Test with default constructor
 		System.out.print("Hw1Progression with default constructor: ");
 		Prog = new Progression_1(); //Declare and initialize
 		Prog.printProgression(10); // Print the first 10 values on the screen
 		
-		// Test Hw1Progression with 5 as the start value
+		// Test with 5 as the start value
 		System.out.print("Hw1Progression with 5 as the start value: ");
 		Prog = new Progression_1(5);
 		Prog.printProgression(10); //Print the first 10 values on the screen
